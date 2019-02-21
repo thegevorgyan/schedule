9df9a1e146f5c07046dd7966242b6834af48b8e4
Artak <g.artak@yahoo.com>
2019-02-21 15:42:54 -0800

    normal version

--- resources/js/components/EmployeeAccount.vue
+++ resources/js/components/EmployeeAccount.vue
@@ -55,6 +55,7 @@
             <v-icon v-if="exp_panel[2]==true" slot="header" color="#435b71">visibility</v-icon>
             <v-icon v-if="exp_panel[2]==false" slot="header">visibility_off</v-icon>
             <div v-if="!mini" slot="header">View</div>
+            
           </v-expansion-panel-content>
       </v-expansion-panel>
     </v-navigation-drawer>
@@ -92,27 +93,25 @@
             counter
             @click:append="showPwd1 = !showPwd1;"
           ></v-text-field> 
-           <v-checkbox
+          <!-- <v-checkbox
             v-model="info.change_pwd"
             label="I want to change my password:"
-          ></v-checkbox> 
+          ></v-checkbox> -->
 
 
 
-   <v-form ref="form">
+
           <v-text-field
             v-if="info.change_pwd==true"
             v-model="new_pwd"
             :append-icon="showPwd2 ? 'visibility_off' : 'visibility'"
-           :rules="confirmPassword"
+            :rules="[rules.required]"
             :type="showPwd2 ? 'text' : 'password'"
             name="input-10-1"
             label="New password"           
             counter
             @click:append="showPwd2 = !showPwd2;"
           ></v-text-field> 
-           </v-form>
-       
           <v-text-field
             v-if="info.change_pwd==true"
             v-model="confirm_new_pwd"
@@ -124,7 +123,6 @@
             counter
             @click:append="showPwd3 = !showPwd3;"
           ></v-text-field> 
-                   
 
 
         <v-btn
@@ -230,10 +228,6 @@
     //  this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
     },
     watch: {
-
-       new_pwd:'validateField',
-       confirm_new_pwd:'validateField'
-
     /*  new_pwd: function (newQuestion, oldQuestion) {
         this.new_pwd_value = newQuestion;
       //  this.debouncedGetAnswer()
@@ -250,9 +244,6 @@ console.log(this.rules.same(false));
       }*/
     },
     methods: {
-      validateField () {
-       this.$refs.form.validate()
-      },
       esAlert: function(value, type, text){        
         this.es_alert = value;
         this.es_alert_type = type;
@@ -302,7 +293,7 @@ console.log(this.rules.same(false));
         });
       },
 
-      
+
 
 
 
@@ -354,7 +345,6 @@ console.log(this.rules.same(false));
       }, 
 
 
-      
 
 
 
@@ -369,23 +359,8 @@ console.log(this.rules.same(false));
 
 
 
-    },
-    computed: {
-     confirmPassword () {
-      const confirmPassword = []
 
-       if (this.confirm_new_pwd) {
-         const rule =
-           v => (!!v && v) === this.confirm_new_pwd ||
-             'Values do not match'
-
-         confirmPassword.push(rule)
-       }
-
-      return confirmPassword
-     }
-   }
-
+    }
   }
 </script>
 
